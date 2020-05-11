e08ecd98f6ed72f10e4665deb70135e418247b74
liyan <18640584088@163.com>
2020-05-11 14:56:37 +0800

    完成遍历省市县三级列表功能

--- app/src/main/java/com/coolweather/android/db/Province.java
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -1,6 +1,8 @@
 package com.coolweather.android.db;
 
-public class Province {
+import org.litepal.crud.LitePalSupport;
+
+public class Province extends LitePalSupport {
     private int id;
     private String provinceName;
     private int provinceCode;
@@ -28,4 +30,5 @@ public class Province {
     public void setProvinceCode(int provinceCode) {
         this.provinceCode = provinceCode;
     }
+
 }
